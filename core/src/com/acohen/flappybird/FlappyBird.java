d688a7457be91efe46db0bdf9b46d2ba5762a9d1
Andrew Cohen <dfman18@gmail.com>
2019-02-17 22:13:09 -0700

    Initial commit

---
+++ core/src/com/acohen/flappybird/FlappyBird.java
@@ -0,0 +1,80 @@
+package com.acohen.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+
+    SpriteBatch batch;
+    Texture background;
+    Texture[] birds;
+    int flapState = 0;
+    float birdY = 0;
+    float velocity = 0;
+    int gameState = 0;
+    float gravity = 2;
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        background = new Texture("bg.png");
+        birds = new Texture[2];
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+        birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
+    }
+
+    @Override
+    public void render () {
+        if (gameState != 0) {
+            if (Gdx.input.justTouched()) {
+                velocity = -30;
+            }
+            if (birdY > 0 || velocity < 0) {
+                velocity = velocity + gravity;
+                birdY -= velocity;
+            }
+        } else {
+            if (Gdx.input.justTouched()) {
+                gameState = 1; // start playing
+            }
+        }
+
+        if (flapState == 0) {
+            flapState = 1;
+        } else {
+            flapState = 0;
+        }
+
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
+        batch.end();
+    }
+
+//    SpriteBatch batch;
+//	Texture img;
+//
+//	@Override
+//	public void create () {
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
+//	}
+//
+//	@Override
+//	public void render () {
+////		Gdx.gl.glClearColor(1, 0, 0, 1);
+////		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+////		batch.begin();
+////		batch.draw(img, 0, 0);
+////		batch.end();
+//	}
+//
+//	@Override
+//	public void dispose () {
+//		batch.dispose();
+//		img.dispose();
+//	}
+}
